0006c20eedad58c711916de7f618fc71ad922ae7
Justin Hanson <justin.hanson.2002@gmail.com>
2025-03-07 13:52:36 -0700

    This should commit the weekly changes

--- FinalProject.py
+++ FinalProject.py
@@ -1,1 +1,35 @@
-print("hello world")
+class linkedListNode:
+    def __init__(self, payload, nextNode = None):
+        self.dataload = payload
+        self.nextNode = nextNode
+        
+
+#MAIN
+nodeA = linkedListNode('Binil')
+nodeB = linkedListNode('Sam')
+nodeC = linkedListNode('Rachel')
+nodeD = linkedListNode('Tamara')
+
+
+nodeA.nextNode = nodeB
+nodeB.nextNode = nodeC
+nodeC.nextNode = nodeD
+
+currentNode = nodeA
+while currentNode.nextNode is not None:
+    print(f"The friend of {currentNode.dataload} = {currentNode.nextNode.dataload}")
+    currentNode = currentNode.nextNode
+if currentNode.nextNode is None:
+    print(f"The friend of {currentNode.dataload} = None")
+
+
+
+currentNode = nodeA
+while True:
+    print(f"{currentNode.dataload}")
+    if currentNode.nextNode is not None:
+        print(f"The friend of {currentNode.dataload} = ")
+    currentNode = currentNode.nextNode
+    if currentNode.nextNode is None:
+        print("None")
+        break
